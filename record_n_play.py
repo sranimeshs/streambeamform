70d3133ab229639ffcc2f0120dda342426f0ceff
animesh <animesh@brainoft.com>
2017-12-18 18:02:02 -0800

    streaming beamforming

--- record_n_play.py
+++ record_n_play.py
@@ -16,8 +16,6 @@ SAMPLE_RATE = 44100
 NUM_CHANNELS = 2
 # Number of buffers for computing correlation
 NUM_BUFF = 3
-# Size of buffer
-BUFF_SIZE = 1024
 
 class WavBuffer(object):
     def __init__(self, datatype):
@@ -45,8 +43,13 @@ class BeamFormer(object):
     def __init__(self, datatype, delay_list):
         self.delays = delay_list
 
+        # Initialize buffer for left and right channels
         self.left_buff = WavBuffer(datatype)
         self.right_buff = WavBuffer(datatype)
+
+        # Insert an empty buffer to begin with
+        self.left_buff.insert(np.zeros((BUFF_SIZE,), dtype=datatype))
+        self.right_buff.insert(np.zeros((BUFF_SIZE,), dtype=datatype))
 
         #self.shifted = {delay: np.zeros((BUFF_SIZE,), dtype=datatype) for delay in self.delays}
 
@@ -70,25 +73,33 @@ class BeamFormer(object):
 
             # Get a temporary right buffer and shift it accordingly
             temp_right = np.zeros((NUM_BUFF * BUFF_SIZE,), dtype=right_wav.dtype)
-            # Right buffer lags behind left buffer
+            # If the lag is negative that means that the right buffer should lag behind left buffer
+            # for better correlation.
             if opt_delay < 0:
-                temp_right[-opt_delay:] = self.right_buff.buff[:self.right_buff.size + opt_delay]
-            # Right buffer leads the left buffer
+                temp_right[:NUM_BUFF * BUFF_SIZE + opt_delay] = self.right_buff.buff[-opt_delay:]
+            # We should shift the right buffer to leading position.
             elif opt_delay > 0:
-                temp_right[:len(temp_right) - opt_delay] = self.right_buff.buff[opt_delay:]
+                temp_right[opt_delay:] = self.right_buff.buff[:self.right_buff.size - opt_delay]
 
+            # The middle buffer is the buffer of interest
+            l = self.left_buff.buff[(NUM_BUFF/2) * BUFF_SIZE: (NUM_BUFF/2 + 1) * BUFF_SIZE]
+            r = temp_right[(NUM_BUFF/2) * BUFF_SIZE: (NUM_BUFF/2 + 1) * BUFF_SIZE]
             # Return the last BUFF_SIZE of beamformed data
-            return (self.left_buff.buff[(NUM_BUFF-1)*BUFF_SIZE:NUM_BUFF*BUFF_SIZE] +
-                    temp_right[(NUM_BUFF-1)*BUFF_SIZE:NUM_BUFF*BUFF_SIZE])/2
+            return (l+r)/2
         else:
             # We are bootstrapping. Send the average of the mono channels.
-            return (left_wav + right_wav)/2
+            return None
 
 
 @click.command()
 @click.option('--inp_wav_file', type=str, default="Wav file", help="Fullpath of the input wav file")
 @click.option('--out_wav_file', type=str, default="Wav file", help="Fullpath of the output wav file")
-def replay_and_record(inp_wav_file, out_wav_file):
+@click.option('--buff_size', type=str, default="buffer size", help="Amount of data to be read from wav file at a time")
+@click.option('--stats_file', type=str, default="Text file", help="Fullpath of the file containing stats")
+def replay_and_record(inp_wav_file, out_wav_file, buff_size, stats_file):
+
+    global BUFF_SIZE
+    BUFF_SIZE = int(buff_size)
 
     # Get the data and load it into the memory so that we don't read it from the file
     # all the time.
@@ -104,9 +115,9 @@ def replay_and_record(inp_wav_file, out_wav_file):
     beamformer = BeamFormer(leftwav.dtype, delay_list)
 
     frames_read = 0
+    frames_written = 0
     time_taken_per_buffer = []
     while len(leftwav) - frames_read > BUFF_SIZE:
-        #print('frames read %d out of %d frames' % (frames_read, len(leftwav)))
         frame_end_ind = frames_read + min(BUFF_SIZE, len(leftwav) - frames_read)
         left_frames = leftwav[frames_read : frame_end_ind]
         right_frames = rightwav[frames_read : frame_end_ind]
@@ -115,14 +126,16 @@ def replay_and_record(inp_wav_file, out_wav_file):
         mono_out = beamformer.get_beamformed_data(left_frames, right_frames)
         end_beamforming = time.time()
 
-        outwav[frames_read:frame_end_ind] = mono_out;
-        frames_read = frame_end_ind
+        if frames_read > 0:
+            outwav[frames_written:frames_written + BUFF_SIZE] = mono_out;
+            frames_written += BUFF_SIZE
 
+        frames_read = frame_end_ind
         time_taken_per_buffer.append((end_beamforming - start_beamforming))
 
     wavfile.write(out_wav_file, rate, outwav)
 
-    with open('stats.txt', 'w') as fw:
+    with open(stats_file, 'w') as fw:
         for stat in time_taken_per_buffer:
             fw.write(str(stat) + '\n')
 
